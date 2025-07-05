9b704a1105ce0d784cbad2efbab125daf9cf4e78
Mor1onzzz <dong2172go@gmail.com>
2025-07-05 13:25:56 +0800

    change config

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,10 +3,10 @@ const isDev = process.env.NODE_ENV === 'development';
 let config = {
   request: {
     token: ['ghp_POKLPJsLlupuS0qR', 'JZ7aYwuMXivIgG4Ne353'],
-    clientID: isDev ? 'Ov23liR3JFjYgqlvKfNT' : '694df1779e48d5a450d3',
+    clientID: isDev ? 'Ov23liR3JFjYgqlvKfNT' : 'Ov23liR3JFjYgqlvKfNT',
     clientSecret: isDev
       ? 'ee095d759fa9c814915a0a18e272e96975d5bae3'
-      : '23420dd29f671adc5107a5565ed47f655f8e1260',
+      : 'ee095d759fa9c814915a0a18e272e96975d5bae3',
     pageSize: 6,
     autoProxy:
       'https://cors-anywhere.azm.workers.dev/https://github.com/login/oauth/access_token',
